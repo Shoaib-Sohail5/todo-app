2e05efed653ed158b9b9f9ae3f19d620f3bd7008
Shoaib Sohail <shoaibsohail.shaik@gmail.com>
2024-08-15 12:14:37 +0530

    input validations

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const { createTodo, updateTodo } = require("./types")
 const app = express()
 const PORT = 3000
 app.use(express.json())
@@ -8,10 +9,24 @@ app.get('/todos', (req, res) => {
 })
 
 app.post('/todo', (req, res) => {
+    const createPayload = req.body
+    const parsePayload = createTodo.safeParse(createPayload)
 
+    if(!parsePayload.success()) {
+        res.status(411).json({
+            msg: "you have send wrong inputs"
+        })
+        
+    }
 })
 
 app.put('/completed', (req, res) => {
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+
+    if(!parsedPayload.success) {
+        res.status(411).
+    }
 
 })
 
